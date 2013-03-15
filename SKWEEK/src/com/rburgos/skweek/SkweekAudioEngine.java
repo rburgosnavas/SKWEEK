5d9ce7158f082a52cf8e02ab5419cb10998ca9c3
rburgosnavas <roby0b@gmail.com>
2013-03-14 22:26:39 -0700

    Refactored some functionalities to separate methods.

--- SKWEEK/src/com/rburgos/skweek/SkweekAudioEngine.java
+++ SKWEEK/src/com/rburgos/skweek/SkweekAudioEngine.java
@@ -1,7 +1,6 @@
 package com.rburgos.skweek;
 
 import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
@@ -10,24 +9,93 @@ import javax.sound.sampled.SourceDataLine;
 public class SkweekAudioEngine implements Runnable
 {
 
-	AudioFormat audioformat;
-	DataLine.Info info;
-	SourceDataLine sourcedataline;
-	AudioInputStream audioInput;
-	String exp;
-	int t, x = 1, y = 1, z = 1, scale = 1;
-	int loop;
-    byte[] buffer = new byte[8];
-    boolean play = false;
+	private static SkweekAudioEngine audioEngine;
+	private AudioFormat audioFormat;
+	private DataLine.Info info;
+	private SourceDataLine dataLine;
+	private String exp;
+	private int t, x = 1, y = 1, z = 1, scale = 1, res, loop;
+    private byte[] buffer = new byte[8];
+    private boolean play = false;
+    
+    public SkweekAudioEngine() throws LineUnavailableException
+    {
+    	t = 0;
+		audioFormat = new AudioFormat(11025f, 8, 2, false, false);
+        info = new DataLine.Info(SourceDataLine.class, audioFormat);
+        dataLine = (SourceDataLine) AudioSystem.getLine(info);  	
+    }
+    
+    public static SkweekAudioEngine getEngine() throws LineUnavailableException
+    {
+    	if (audioEngine == null)
+    	{
+    		audioEngine = new SkweekAudioEngine();
+    	}
+    	return audioEngine;
+    }
+
+	@Override
+	public void run()
+	{
+		try
+        {
+	        play();
+        }
+        catch (LineUnavailableException e)
+        {
+	        e.printStackTrace();
+        }
+		write();
+		stop();
+	}
+	
+	private void play() throws LineUnavailableException
+	{
+        dataLine.open(audioFormat, dataLine.getBufferSize());  
+		dataLine.start();
+		
+		System.out.println("+ thread started");
+		System.out.println("+ buffer size: " + dataLine.getBufferSize());
+	}
+	
+	private void stop()
+	{
+        dataLine.stop();
+        dataLine.flush();
+        dataLine.close();
+
+		System.out.println("- thread stopped");
+        System.out.println("x thread closed\n");
+	}
+	
+	private void write()
+	{
+		while (play)
+		{
+		    for (int i = 0; i < (buffer.length); i++)
+		    {
+		        t++;
+		    	res = SkweekParser.evalToInt(exp, t, x, y, z) / tInc(scale);
+		    	buffer[i] = (byte) res;
+		        
+		        if (t > (buffer.length*loop))
+		        {
+		        	t = 0;
+		        }
+		    }
+		    dataLine.write(buffer, 0, buffer.length);
+		}
+	}
 	
 	public void setExp(String exp)
 	{
 		this.exp = exp;
 	}
 	
-	public void setPlay(boolean set)
+	public void setPlay(boolean setPlay)
 	{
-		this.play = set;
+		this.play = setPlay;
 	}
 	
 	public void setLoop(int loop)
@@ -58,51 +126,6 @@ public class SkweekAudioEngine implements Runnable
 	public int tInc(int scale)
 	{
 		return scale > 0 ? scale : this.scale;
-	}
-
-	@Override
-	public void run()
-	{
-		System.out.println("+ thread started");
-		t = 0;
-		try
-        {
-	        audioformat = new AudioFormat(11025f, 8, 2, false, false);
-	        info = new DataLine.Info(SourceDataLine.class, audioformat);
-	        sourcedataline = (SourceDataLine) AudioSystem.getLine(info);
-	        sourcedataline.open(audioformat, sourcedataline.getBufferSize());
-	        sourcedataline.start();
-	        
-	        System.out.println("+ buffer size: " + sourcedataline.getBufferSize());
-	        
-	        while (play)
-	        {
-		        for (int i = 0; i < (buffer.length); i++)
-		        {
-			        buffer[i] = 
-			        		(byte) (SkweekParser.evalToInt(exp, t++, x, y, z) / 
-			        				tInc(scale));
-			        
-			        if (t > (buffer.length*loop)-t)
-			        {
-			        	t = 0;
-			        }
-		        }
-		        sourcedataline.write(buffer, 0, buffer.length);
-	        }
-	        
-	        System.out.println("- thread stopped");
-        }
-        catch (LineUnavailableException e)
-        {
-	        e.printStackTrace();
-        }
-        // sourcedataline.drain();
-        sourcedataline.flush();
-        sourcedataline.stop();
-        sourcedataline.close();
-        
-        System.out.println("x thread closed\n");
 	}
 
 }
